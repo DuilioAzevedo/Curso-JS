7ada287cbb07f1a6e6aa11a9da410293c1a65217
DuilioAzevedo <duilio_uei@hotmail.com>
2022-03-16 15:59:23 -0300

    aula11

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 100
+
+console.log (`Sua velocidade é de ${vel} km/h`)
+ if (vel > 60) {
+     console.log('MULTADO!!! Sua velocidade está acima do limite')
+ }
+ console.log('Dirija sempre com segurança')
---
+++ aula 11/ex009.js
@@ -0,0 +1,9 @@
+var nacionalidade = 'Canada'
+console.log (`Voce nasceu no ${nacionalidade}` )
+
+if (nacionalidade == "Brasil"){
+    console.log("Brasileiro: Acesso liberado ")
+}
+else{
+    console.log ('Estrangeiro: Acesso negado')
+}
