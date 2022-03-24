3dceb1a14c96ba6447bea30dd4b9996d7df3148d
DuilioAzevedo <duilio_uei@hotmail.com>
2022-03-24 13:44:44 -0300

    aula 13

---
+++ aula 13/ambiente.js
@@ -0,0 +1,19 @@
+var c = 1
+
+do{
+    console.log (`passo ${c}`)
+    c++
+}while(c<7)
+
+
+/*while(c<=6){
+    console.log(`passo ${c}`)
+    c++
+} */
+
+
+
+/*console.log ('passo 1')
+console.log ('passo 2')
+console.log ('passo 3')
+console.log ('passo 4')*/
