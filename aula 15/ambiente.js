e061e544da0f2d8becf3197ead569c18e05ad98c
DuilioAzevedo <duilio_uei@hotmail.com>
2022-03-30 15:55:29 -0300

    aula 16

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -4,7 +4,6 @@ num.sort()
 console.log(num)
 console.log (`O vetor tem ${num.length} posições`)
 console.log (`O primeiro valor do vetor é ${num[0]} `)
-
 let pos = num.indexOf(5)
 if(pos == -1){
     console.log(`O valor não foi encontrado!`)
