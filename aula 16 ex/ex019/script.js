0cd3b34d9dc350d4f784f447cf0f2e972256c058
DuilioAzevedo <duilio_uei@hotmail.com>
2022-04-06 13:16:51 -0300

    finished ex016

--- aula 16 ex/ex019/script.js
+++ aula 16 ex/ex019/script.js
@@ -1,42 +1,43 @@
 let tab =[]
+let res = document.getElementById("res")
 
     function adiciona(){
+    res.innerHTML =''
     let num = document.getElementById("numtxt")
     let n = Number(num.value)
     let existe = tab.indexOf(n)
     if(num.value.length==0){
         alert("Digite um número")
-
-
+    }else if(n<1 ||n>100){
+        alert("Valor fora do padrão")
     }else if (existe == -1){ 
         tab.push(n)
+        let sel = document.getElementById("seltab")
+        let item = document.createElement('option')
+        item.text = `O valor ${n} foi adicionado`
+        sel.appendChild(item)
     }else{
 
         alert("Número já foi adicionado anteriormente")
     }
    
-  
+  num.value = ''
+  num.focus()
 }
 
   function finaliza(){
-/**let sel = document.getElementById("seltab")
-    let qntd = document.createElement('option')
-    sel.innerHTML = ''
-
-    qntd.value = `${tab.length}`
-    sel.appendChild(qntd) */
-    
-    console.log (`Foram cadastrados ${tab.length} numeros`)
-    tab.sort()
-    //console.log (`O maior valor informado é ${tab.slice(-1)[0]}`)
-    console.log (`O maior valor informado é ${tab[tab.length -1]}`)
-    console.log (`O menor valor informado é ${tab[0]}`)
+    tab.sort((a,b)=>a-b) //como ordenar numeros
     let soma = 0
     for (let pos =0; pos < tab.length; pos++){
         soma += tab[pos]
     }
-    console.log(`À soma de todos os valores é ${soma}`)
     let media = (soma/tab.length)
-    console.log(`A média dos valores informados é de: ${media}`)
+    
+    res.innerHTML = `Foram cadastrados ${tab.length} numeros <br>
+    O maior valor informado é ${tab[tab.length -1]} <br>
+    O menor valor informado é ${tab[0]}<br>
+    À soma de todos os valores é ${soma}<br>
+    A média dos valores informados é de: ${media}`
+    
 
 }
